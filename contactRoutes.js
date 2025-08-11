05fdb9a963402e16a2ff9aeeb3247a678f0821dc
mahmoud ali khalil <120006261+kikmod@users.noreply.github.com>
2025-08-11 13:46:59 +0300

    Delete contactRoutes.js

--- contactRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { submitContactForm } = require('../Controller/contactController');
-
-router.post('/contact', submitContactForm);
-
-module.exports = router;
